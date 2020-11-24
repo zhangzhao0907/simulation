375f6ddbd926527a68aefd959b9f888408e416f6
zhangzhao0907 <zhangzhao0907@163.com>
2020-11-24 23:30:33 +0800

    modify the initiation

--- src/Initiation.js
+++ src/Initiation.js
@@ -5,8 +5,9 @@ import {
   Radio,
   Button,
   Input,
-  DatePicker,
-  Divider,
+  Image,
+  Row,
+  Col,
 } from 'antd';
 
 import React from 'react';
@@ -32,12 +33,12 @@ const Initiation = () => {
       firstName: 'test',
       lastName: 'test'
     }).then(function (response) {
-        console.log(response);
-      })
+      console.log(response);
+    })
       .catch(function (error) {
         console.log(error);
       });
-      history.push('/success');
+    history.push('/success');
   };
 
   return (
@@ -45,166 +46,176 @@ const Initiation = () => {
       name="initiation"
       {...formItemLayout}
       onFinish={onFinish}
+      size='small'
       initialValues={{
         ['age']: 25,
         ['bmi']: 18.5,
         ['weight']: 60,
       }}>
       <p className="banner">Initiate a Simulation</p>
-      <Divider style={{ margin: "0 0 50px" }} />
-      <Form.Item name="age" label="Age 年龄：">
-        <InputNumber min={1} max={100} />
-      </Form.Item>
 
-      <Form.Item name="gender" label="Gender 性别：">
-        <Radio.Group>
-          <Radio value="男">男</Radio>
-          <Radio value="女">女</Radio>
-        </Radio.Group>
-      </Form.Item>
+      <Row gutter={[0, 16]}>
+        <Col span={24} style={{ padding: 0, overflow: "hidden" }}>
+          <Image width={300} height={220} src="init1.jpg" className="image-style"></Image>
+          <div class="shader shader1">
+            <h1>Patient</h1>
+          </div>
 
-      <Form.Item name="wound_location" label="Wound Location 伤疤位置：">
-        <Input placeholder="please enter your wound location" style={{ width: "400px" }} />
-      </Form.Item>
+          <div className="div-style div1" style={{ height: "220px" }}>
+            <Form.Item name="age" label='Age年龄' style={{ color: "white" }}>
+              <InputNumber min={1} max={100} />
+            </Form.Item>
 
-      <Form.Item name="bmi" label="BMI：">
-        <InputNumber min={1} max={300} />
-        <span className="span-text">(BMI index)</span>
-      </Form.Item>
+            <Form.Item name="gender" label="Gender 性别：">
+              <Radio.Group>
+                <Radio value="男">男</Radio>
+                <Radio value="女">女</Radio>
+              </Radio.Group>
+            </Form.Item>
 
-      <Form.Item name="weight" label="Weight 体重：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(kg)</span>
-      </Form.Item>
+            <Form.Item name="bmi" label="BMI：">
+              <InputNumber min={1} max={300} />
+              <span className="span-text">(BMI index)</span>
+            </Form.Item>
 
-      <Form.Item name="wound_width" label="Wound Width 伤疤宽度：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(mm)</span>
-      </Form.Item>
-
-      <Form.Item name="wound_depth" label="Wound Depth 伤疤深度：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(mm)</span>
-      </Form.Item>
-
-      <Form.Item name="skin_tension" label="Skin Tension 皮肤张力：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="tgf" label="TGF-β Concentraion：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="tnf" label="TNF-ɑ Concentraion：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="inflammatory_cytokine" label="Inflammatory Cytokine Concentraion：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="inflammatory_cell" label="Inflammatory Cell Concentraion：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="macrophages_concentraion" label="Macrophages Concentraion：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="wound_healing_time" label="Wound Healing Time 治疗时间：">
-        <DatePicker />
-        <span className="span-text">(Please select a date)</span>
-      </Form.Item>
-
-      <Form.Item name="wound_infected" label="Wound Infected 伤口感染：">
-        <Radio.Group>
-          <Radio value="是">是</Radio>
-          <Radio value="否">否</Radio>
-        </Radio.Group>
-      </Form.Item>
-
-      <Form.Item name="epidermal_laser_density" label="Epidermal Laser Density：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="dermal_laser_density" label="Dermal Laser Density：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="laser_diameter" label="Laser Diameter：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(mm)</span>
-      </Form.Item>
-
-      <Form.Item name="laser_cross" label="Laser Cross Section Shape：">
-        <Input placeholder="please describe your Laser Cross Section Shape" />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="laser_inclination_angle" label="Laser Inclination Angle：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="duration_laser" label="Duration Between Laser Treatment：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="betamethason_dosage" label="Betamethason Dosage：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="pressure" label="Pressure：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="tension_reduction" label="Tension Reduction：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="botulinum_toxin" label="Botulinum Toxin Type A Dosage：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="tgf_antibody" label="TGF-β1 Antibody Dosage：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="avastin_dosage" label="Avastin Dosage：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
-
-      <Form.Item name="inhibitor_dosage" label="Inhibitor Dosage：">
-        <InputNumber min={1} max={150} />
-        <span className="span-text">(the discription of the parameter)</span>
-      </Form.Item>
+            <Form.Item name="weight" label="Weight 体重：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(kg)</span>
+            </Form.Item>
+          </div>
+        </Col>
+      </Row>
 
 
-      <Form.Item
-        wrapperCol={{
-          span: 12,
-          offset: 10,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Simulation
-        </Button>
-      </Form.Item>
+      <Row gutter={[0, 16]} style={{ marginTop: "25px" }}>
+        <Col span={24} style={{ padding: 0, overflow: "hidden" }}>
+          <Image width={300} height={220} src="init2.jpg" className="image-style"></Image>
+          <div class="shader shader2">
+            <h1>Wound</h1>
+          </div>
+
+          <div className="div-style div2" style={{ height: "220px" }}>
+            <Form.Item name="wound_width" label="Wound Width 疤痕宽度：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(mm)</span>
+            </Form.Item>
+
+            <Form.Item name="wound_depth" label="Wound Depth 疤痕深度：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(mm)</span>
+            </Form.Item>
+
+            <Form.Item name="wound_location" label="Wound Location 疤痕位置：">
+              <Input placeholder="please enter wound location" style={{ width: "200px" }} />
+            </Form.Item>
+
+            <Form.Item name="wound_infected" label="Wound Infected 伤口感染：">
+              <Radio.Group>
+                <Radio value="是">是</Radio>
+                <Radio value="否">否</Radio>
+              </Radio.Group>
+            </Form.Item>
+          </div>
+        </Col>
+      </Row>
+
+      <Row gutter={[0, 16]} style={{ marginTop: "25px" }}>
+        <Col span={24} style={{ padding: 0, overflow: "hidden" }}>
+          <Image width={300} height={360} src="init3.jpg" className="image-style"></Image>
+          <div class="shader shader3">
+            <h1>Laser</h1>
+          </div>
+
+          <div className="div-style div3" style={{ height: "360px" }}>
+            <Form.Item name="epidermal_laser_density" label="Epidermal Laser Density：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(the discription of the parameter)</span>
+            </Form.Item>
+
+            <Form.Item name="dermal_laser_density" label="Dermal Laser Density：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(the discription of the parameter)</span>
+            </Form.Item>
+
+            <Form.Item name="laser_diameter" label="Laser Diameter：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(mm)</span>
+            </Form.Item>
+
+            <Form.Item name="laser_cross" label="Laser Cross Section Shape：">
+              <Input placeholder="Please Select" style={{ width: "200px" }} />
+            </Form.Item>
+
+            <Form.Item name="laser_inclination_angle" label="Laser Inclination Angle：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(degree)</span>
+            </Form.Item>
+
+            <Form.Item name="duration_laser" label="Duration Between Laser Treatment：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(days)</span>
+            </Form.Item>
+
+            <Form.Item name="betamethason_dosage" label="Betamethason Dosage：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(the discription of the parameter)</span>
+            </Form.Item>
+          </div>
+        </Col>
+      </Row>
+
+
+      <Row gutter={[0, 16]} style={{ marginTop: "25px" }}>
+        <Col span={24} style={{ padding: 0, overflow: "hidden" }}>
+          <Image width={300} height={315} src="init4.jpg" className="image-style"></Image>
+          <div class="shader shader4">
+            <h1>Intervention</h1>
+          </div>
+
+          <div className="div-style div4" style={{ height: "315px" }}>
+            <Form.Item name="pressure" label="Pressure：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(the discription of the parameter)</span>
+            </Form.Item>
+
+            <Form.Item name="tension_reduction" label="Tension Reduction：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(the discription of the parameter)</span>
+            </Form.Item>
+
+            <Form.Item name="botulinum_toxin" label="Botulinum Toxin Type A Dosage：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(the discription of the parameter)</span>
+            </Form.Item>
+
+            <Form.Item name="tgf_antibody" label="TGF-β1 Antibody Dosage：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(the discription of the parameter)</span>
+            </Form.Item>
+
+            <Form.Item name="avastin_dosage" label="Avastin Dosage：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(the discription of the parameter)</span>
+            </Form.Item>
+
+            <Form.Item name="inhibitor_dosage" label="Inhibitor Dosage：">
+              <InputNumber min={1} max={150} />
+              <span className="span-text">(the discription of the parameter)</span>
+            </Form.Item>
+          </div>
+        </Col>
+      </Row>
+
+
+      <Row gutter={[16, 16]}>
+        <Col span={24}>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{width:'100%'}}>
+              Simulation
+            </Button>
+          </Form.Item>
+        </Col>
+      </Row>
     </Form>
   );
 };
